bc3892214d456d7c547c936bdb513bf5d969b0fa
Joseph Utecht <jrutecht@uams.edu>
2019-05-09 10:19:51 -0400

    added initializer

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+regex='\/home\/([^\/]+)\/hpc_tut'
+[[ $(pwd) =~ $regex ]]
+echo "What email would you like to be notified at?"
+read email
+echo "Changing your path to ${BASH_REMATCH[1]}"
+for f in steps/*.step; do
+  sed -i 's/<PATH>/${BASH_REMATCH[1]}/g' $f
+  sed -i 's/<EMAIL>/$email/g' $f
+done
+echo "Downloading Sample Data"
 wget -O data/sample-metadata.tsv https://data.qiime2.org/2019.4/tutorials/moving-pictures/sample_metadata.tsv
 wget -O data/emp-single-end-sequences/sequences.fastq.gz https://data.qiime2.org/2019.4/tutorials/moving-pictures/emp-single-end-sequences/sequences.fastq.gz
 wget -O data/emp-single-end-sequences/barcodes.fastq.gz https://data.qiime2.org/2019.4/tutorials/moving-pictures/emp-single-end-sequences/barcodes.fastq.gz
